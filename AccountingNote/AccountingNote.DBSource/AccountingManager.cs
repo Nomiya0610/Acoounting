c9ebd3854882d8ce990818d6376992fc9f95a361
Nomoya <sad0043138856@gmail.com>
2021-07-30 08:46:49 +0800

    project init

---
+++ AccountingNote/AccountingNote.DBSource/AccountingManager.cs
@@ -0,0 +1,251 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AccountingNote.DBSource
+{
+    public class AccountingManager
+    {
+        public static string GetConnectionString()
+        {
+            //string val =
+            //    ConfigurationManager.AppSettings["ConnectionString"];
+            string val =
+                ConfigurationManager.ConnectionStrings["DefaultConnection"].ConnectionString;
+            return val;
+        }
+
+        public static DataTable GetAccountingList(string userID)
+        {
+            string connStr = GetConnectionString();
+            string dbCommand =
+                $@" SELECT
+                        ID,
+                        Caption,
+                        Amount,
+                        ActType,
+                        CreateDate
+                    FROM Accounting
+                    WHERE UserID = @userID
+                ";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand comm = new SqlCommand(dbCommand, conn))
+                {
+                    comm.Parameters.AddWithValue("@userID", userID);
+
+                    try
+                    {
+                        conn.Open();
+                        var reader = comm.ExecuteReader();
+
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+
+                        return dt;
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.WriteLog(ex);
+                        return null;
+                    }
+                }
+            }
+
+
+        }
+
+
+        public static void CreateAccounting(string userID, string caption, int amount, int actType, string body)
+        {
+            if (amount < 0 || amount > 1000000)
+                throw new ArgumentException("Amount must between 0 and 1,000,000.");
+            
+            if (actType < 0 || actType > 1)
+                throw new ArgumentException("ActType must be 0 or 1. ");
+
+
+                string connStr = GetConnectionString();
+                string dbCommand =
+                    $@" INSERT INTO [dbo].[Accounting]
+                        (
+                            UserID
+                            ,Caption
+                            ,Amount
+                            ,ActType
+                            ,CreateDate
+                            ,Body
+                        )
+                        VALUES
+                        (
+                             @userID
+                            ,@caption
+                            ,@amount
+                            ,@actType
+                            ,@createDate
+                            ,@body
+
+                        )";
+
+
+                using (SqlConnection conn = new SqlConnection(connStr))
+                {
+                    using (SqlCommand comm = new SqlCommand(dbCommand, conn))
+                    {
+                        comm.Parameters.AddWithValue("@userID", userID);
+                        comm.Parameters.AddWithValue("@caption", caption);
+                        comm.Parameters.AddWithValue("@amount", amount);
+                        comm.Parameters.AddWithValue("@actType", actType);
+                        comm.Parameters.AddWithValue("@createDate", DateTime.Now);
+                        comm.Parameters.AddWithValue("@body", body);
+
+                        try
+                        {
+                            conn.Open();
+                            comm.ExecuteNonQuery();   
+
+                        }
+                        catch (Exception ex)
+                        {
+                            Logger.WriteLog(ex);
+
+                        }
+                    }
+                }
+            
+        }
+
+
+        public static void UpdateAccounting(int id, string userID, string caption, int amount, int actType, string body)
+        {
+            if (amount < 0 || amount > 1000000)
+                throw new ArgumentException("Amount must between 0 and 1,000,000.");
+
+            if (actType < 0 || actType > 1)
+                throw new ArgumentException("ActType must be 0 or 1. ");
+
+
+            string connStr = GetConnectionString();
+            string dbCommand =
+                $@" IUPDATE [Accounting]
+                    SET
+                         UserID       = @userID
+                         ,Caption     = @caption
+                         ,Amount      = @amount
+                         ,ActType     = @actType
+                         ,CreatteDate = @creatteDate    
+                         ,Body        = @body    
+                    WHERE 
+                        ID = @id ";
+
+            //connect db & execute
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand comm = new SqlCommand(dbCommand, conn))
+                {
+                    comm.Parameters.AddWithValue("@userID", userID);
+                    comm.Parameters.AddWithValue("@caption", caption);
+                    comm.Parameters.AddWithValue("@amount", amount);
+                    comm.Parameters.AddWithValue("@actType", actType);
+                    comm.Parameters.AddWithValue("@createDate", DateTime.Now);
+                    comm.Parameters.AddWithValue("@body", body);
+
+                    try
+                    {
+                        conn.Open();
+                        comm.ExecuteNonQuery();  
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.WriteLog(ex);
+                    }
+                }
+            }
+        }
+
+
+        public static void DeleteAccounting(int ID)
+        {
+           
+
+            string connStr = GetConnectionString();
+            string dbCommand =
+                $@" DELETE FROM [dbo].[Accounting] 
+                    WHERE ID = @id ";
+
+            //connect db & execute
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand comm = new SqlCommand(dbCommand, conn))
+                {
+                    comm.Parameters.AddWithValue("@id", ID);
+
+                    try
+                    {
+                        conn.Open();
+                        comm.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        Logger.WriteLog(ex);
+                    }
+                }
+            }
+        }
+
+        public static DataRow GetAccounting(int id, string userID)
+        {
+            string connStr = GetConnectionString();
+            string dbCommand =
+                                $@"
+                                 SELECT 
+                                        ID,
+                                        Caption,
+                                        Amount,
+                                        ActType,
+                                        CreateDate,
+                                        Body
+                                 FROM Accounting
+                                 WHERE ID = @id AND UserID = @userID
+
+                                ";
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                using (SqlCommand comm = new SqlCommand(dbCommand, conn))
+                {
+                    comm.Parameters.AddWithValue("@id", id);
+                    comm.Parameters.AddWithValue("@userID", userID);
+                    try
+                    {
+
+                        conn.Open();
+                        var reader = comm.ExecuteReader();
+
+                        DataTable dt = new DataTable();
+                        dt.Load(reader);
+                        
+
+                        if (dt.Rows.Count == 0)
+                            return null;
+
+                        return dt.Rows[0];
+                    }
+                    catch(Exception ex)
+                    {
+                        Logger.WriteLog(ex);
+                        return null;
+                    }
+
+                }
+            }
+        }
+    }
+}
+
---
+++ AccountingNote/AccountingNote/Login.aspx.cs
@@ -0,0 +1,58 @@
+﻿using AccountingNote.DBSource;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace AccountingNote
+{
+    public partial class Login : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if(this.Session["UserLoginInfo"] != null)//若登入過則導入至資訊面
+            {
+                this.plcLogin.Visible = false;
+                Response.Redirect("/SystemAdmin/UserInfo.aspx");
+            }
+            else
+            {
+                this.plcLogin.Visible = true;
+            }
+           
+        }
+
+        protected void btnLogin_Click(object sender, EventArgs e)
+        {
+            string inp_Account = this.txtAccount.Text;//取得使用者資訊
+            string inp_PWD = this.txtPWD.Text;
+
+            if (string.IsNullOrWhiteSpace(inp_Account) || string.IsNullOrWhiteSpace(inp_PWD))//檢查是否為空
+            {
+                this.ltlMsg.Text = "Account / Password is required. ";
+                return;//一旦發生錯誤就不跑程式
+            }
+            var dr = UserInfoManager.GetUserInfoByAccount(inp_Account);
+            // Check null
+            if(dr == null)
+            {
+                this.ltlMsg.Text = "Account doesn't exists. ";
+                return;
+            }
+            //Check Account / Password
+            if (string.Compare(dr["Account"].ToString(), inp_Account, true) == 0 &&//帳號忽略大小寫的比對
+                string.Compare(dr["PWD"].ToString(), inp_PWD,false) ==0 )//密碼則要比對大小寫
+            {
+                this.Session["Userlogininfo"] = dr["Account"].ToString();//帳號寫到Session
+                Response.Redirect("/SystemAdmin/UserInfo.aspx");//導頁至個人資訊頁
+            }
+            else
+            {
+                this.ltlMsg.Text = "Login fail. Please check Account / Password. ";
+                return;
+            }
+        }
+    }
+}
